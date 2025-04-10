a55723beeb5d04977242a34c1d4e199df8a27778
Emanuel Almeida <eumanuel27@gmail.com>
2025-04-10 17:55:33 -0300

    add sistema_bancario

---
+++ sistema_bancario.py
@@ -0,0 +1,34 @@
+# Depósito
+# Saque
+# Extrato
+# Limite de saques = 3
+# Limite dinheiro por saque = 500
+
+deposito = None
+saque = None
+extrato = ""
+limite_saques = 3
+limite_dinheiro = 500
+
+menu = input("""
+Selecione uma operação
+
+[d] Deposito
+[s] Saque
+[e] Extrato
+[d] Sair
+
+>>>""") 
+
+while True:
+    if menu == "d":
+        
+    elif menu == "s":
+    
+    elif menu == "e":
+        
+    elif menu == "d":
+    
+    else:
+        print("Operação inválida.")
+        
