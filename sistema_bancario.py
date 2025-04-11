88ac6f190d2a011c7aa5f6dd34ef7b80f793ac97
Emanuel Almeida <eumanuel27@gmail.com>
2025-04-11 19:39:16 -0300

    improve sistema_bancario; add funções

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,16 +1,44 @@
-# Depósito
-# Saque
-# Extrato
-# Limite de saques = 3
-# Limite dinheiro por saque = 500
+# Variáveis
 
-saldo = 1800
-deposito = None
-saque = None
-extrato = ""
+saldo = 1000
+extrato = "EXTRATO".center(21, "-")
 limite_saques = 3
 limite_dinheiro = 500
 qtde_saque = 0
+
+# Funções
+
+def depositar(saldo, extrato, valor):
+    if valor <= 0:
+        print("Insira um número válido!")
+        return saldo, extrato
+    else:
+        saldo += valor
+        print(f"Você depositou R$ {valor:.2f} Reais\n")
+        print(f"Seu saldo atual é: R$ {saldo:.2f} Reais\n")
+        extrato += f"- Depósito de R$ {valor:.2f} Reais\n"
+        return saldo, extrato
+    
+    
+def sacar(saldo, extrato, qtde_saque, valor):
+    if valor > limite_dinheiro:
+        print("Valor de saque não pode ser maior que R$ 500.00 Reais\n")
+        return saldo, extrato, qtde_saque
+    elif valor > saldo:
+        print("Você não pode saquar mais dinheiro do que tem!") 
+        return saldo, extrato, qtde_saque
+    elif valor <= 0:
+        print("Insira um número válido!")
+        return saldo, extrato, qtde_saque
+    else:
+        qtde_saque += 1
+        saldo -= valor
+        print(f"Você sacou R$ {valor:.2f} Reais\n")
+        print(f"Seu saldo atual é: R$ {saldo:.2f} Reais\n")
+        extrato += f"- Saque de R$ {valor:.2f} Reais\n"
+        return saldo, extrato, qtde_saque
+    
+# Código
 
 while True:
     
@@ -22,60 +50,43 @@ Selecione uma operação
 [e] Extrato
 [q] Sair
 
->>>""") 
+>>>""").lower() 
     
     # DEPÓSITO
     
     if menu == "d":
         print(f"Seu saldo atual é: R$ {saldo:.2f} Reais\n")
         try:
-            deposito = float(input("Digite o valor de depósito: "))
+            saldo, extrato = depositar(saldo, extrato, float(input("Digite valor de depósito: ")))
         except ValueError:
             print("Digite um número válido!\n")
             continue
-        #deposito = float(input("Digite o valor de depósito: "))
-        saldo += deposito
-        print(f"Você depositou R$ {deposito:.2f} Reais\n")
-        print(f"Seu saldo atual é: R$ {saldo:.2f} Reais\n")
-        extrato += f"- Depósito de R$ {deposito:.2f} Reais\n"
                 
-        
     # SAQUE    
     
     elif menu == "s":
         if qtde_saque >= limite_saques:
             print("Limite diário de saque excedido!")
-            
         
         else:
             print(f"Seu saldo atual é: R$ {saldo:.2f} Reais\n")
             try:
-                saque = float(input("Digite o valor do saque: "))
+                saldo, extrato, qtde_saque = sacar(saldo, extrato, qtde_saque, float(input("Digite o valor do saque: ")))
             except ValueError:
                 print("Digite um número válido!")
                 continue
             
-            if saque > limite_dinheiro:
-                print("Valor de saque não pode ser maior que R$ 500.00 Reais\n")
-                
-
-            elif saque <= limite_dinheiro and saque > 0:
-                qtde_saque += 1
-                saldo -= saque
-                print(f"Você sacou R$ {saque:.2f} Reais\n")
-                print(f"Seu saldo atual é: R$ {saldo:.2f} Reais\n")
-                extrato += f"- Saque de R$ {saque:.2f} Reais\n"
-                
-            
-            else:
-                print("Algo deu errado :(")
+            #else:
+            #    print("Algo deu errado :(")
                 
     # EXTRATO
             
     elif menu == "e":
         if extrato == "":
             print(f"Não há nada no extrato.\nSeu saldo atual é R$ {saldo:.2f} Reais")
-        print(extrato)
+        else:
+            print(extrato)
+            print(f"Seu saldo atual é R$ {saldo:.2f} Reais")
                 
     # SAIR
         
@@ -84,15 +95,7 @@ Selecione uma operação
         if menu == "sim":
             break
         elif menu == "nao":
-            menu = input("""
-Selecione uma operação
-
-[d] Deposito
-[s] Saque
-[e] Extrato
-[q] Sair
-
->>>""") 
+            continue
         else:
             print("Comando não reconhecido!")
             
